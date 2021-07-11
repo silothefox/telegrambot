357fcc308f8b327452d3a4eb8745d705ab4c5dd8
KoiCat <k.welslau@googlemail.com>
2021-07-11 22:30:40 +0200

    Added read from file.

--- welcomemsg.py
+++ welcomemsg.py
@@ -5,6 +5,7 @@ global OFFSET
 OFFSET = 0
 
 botToken = ""
+welcometext = "welcometext.txt"
 
 global requestURL
 global sendURL
@@ -50,8 +51,10 @@ def extract_result(dict):
 def send_message(chatId, message):
     requests.post(sendURL + "?chat_id=" + str(chatId) + "&text=" + message)
 
-
-
+def readfromfile(welcometext):
+    from pathlib import Path
+    text = Path(welcometext).read_text('utf-8')
+    return text
 
 
 while True:
@@ -63,6 +66,5 @@ while True:
             if 'new_chat_participant' in newmessage['message']:
                 membername = newmessage['message']['new_chat_participant']['first_name']
                 chatid = newmessage['message']['chat']['id']
-                send_message(chatid, "Hallöchen, " + membername + " und Willkommen im Rheinland Stammtisch ^-^ Hier in der Gruppe, gibt es wichtige Infos zu unseren Veranstaltungen, Anmeldung zur Teilnahme, Mitfahrgelegenheiten- oder Übernachtungsmöglichkeitensuche und Ähnliches. Für Smalltalk, bitten wir dich unserer flauschigen Labergruppe beizutreten 👉🏻 https://t.me/joinchat/SgLKPOJf3PGUzc9X Für ganz gesprächige und Gamer-Fällbälle, gibt es unseren Discord-Server 👉🏻 https://discord.gg/DGtKpBw Falls du nur Infos willst, findest du hier unseren Info Kanal 👉🏻 https://t.me/Rheinland_Stammtisch_Infokanal. Du siehst, es steht also ganz viel Platz bereit um dich vorzustellen und die Mitglieder kennen zu lernen ^^ Bitte beachte, dass alle aufgelisteten Chats SFW-Chats sind. Desweiteren gelten für alle Chats grundlegende Benimmregeln, die Vermeidung von Rassismus, Mobbing, Beleidigungen aber auch Spam ist nicht erwünscht.  Wenn du eine Anweisung von einem Admin erhälst, bitte halte dich an diese! Grundsätzlich beißen die Admins nicht, wir knabbern im Notfall aber gerne. 😘 Wir wünschen dir viel Spaß!")
-
+                send_message(chatid, readfromfile(welcometext)) 
     sleep(1)
